166ddfb60cc2fcb1275cfbb1a72a5f000a488db7
Jaime Liew <33415790+jaimeliew1@users.noreply.github.com>
2018-06-28 17:01:11 +0200

    split power plot into below and above rated wind speed for clarity

--- Postprocessing/Ch2_power.py
+++ Postprocessing/Ch2_power.py
@@ -43,9 +43,21 @@ def get_pelec_data_from_sim(sim):
 
 
 
-def run(dlc, dlc_noipc, c='ipc07', SAVE=None):
 
-    WSP = np.arange(4, 27, 2)
+def run(dlc, dlc_noipc, SAVE=None):
+    if SAVE:
+        SAVE1 = SAVE[:-4] + '_below_rated.png'
+        SAVE2 = SAVE[:-4] + '_above_rated.png'
+    else:
+        SAVE1 = SAVE2 = None
+    _run(dlc, dlc_noipc, WSP = [4, 6, 8, 10, 12, 14], SAVE=SAVE1)
+    _run(dlc, dlc_noipc, WSP = [ 16, 18, 20, 22, 24, 26], SAVE=SAVE2)
+
+
+
+def _run(dlc, dlc_noipc, WSP= np.arange(4, 27, 2), c='ipc07', SAVE=None):
+
+    #WSP = np.arange(4, 27, 2)
     X = {}
 
     for wsp in WSP:
@@ -59,8 +71,8 @@ def run(dlc, dlc_noipc, c='ipc07', SAVE=None):
         X1[wsp] = get_pelec_data_from_sim(sim)
 
 
-    plt.figure(figsize=[6,4])
-    plt.xticks(np.arange(4, 27, 4))
+    plt.figure(figsize=[4,4])
+    plt.xticks(WSP)
     plt.xlabel('Wind Speed [m/s]')
     plt.ylabel('Electrical Power Output [MW]')
     dx = 0.3
@@ -73,21 +85,21 @@ def run(dlc, dlc_noipc, c='ipc07', SAVE=None):
     a1 = mpatches.Patch(fc=colors[1], ec='k', alpha=0.7, label='No IPC')
     a2 = mpatches.Patch(fc=colors[-1], ec='k', alpha=0.7, label='IPC ($C_{2}$)')
 
-    plt.legend(handles = [a1, a2], loc='lower right')
+    plt.legend(handles = [a1, a2], loc='upper left')
     if SAVE:
         plt.savefig(SAVE, dpi=200, bbox_inches='tight')
     plt.show(); print()
 
 
-    # print LaTeX table
-    for wsp in WSP:
-        print(f'\multirow{{2}}{{*}} {{{wsp}}} & No IPC &{np.mean(X[wsp]):2.3f} &  {np.std(X[wsp]):2.3f} \\\\')
-        print(f'& IPC ($C_2$) & {np.mean(X1[wsp]):2.3f} &  {np.std(X1[wsp]):2.3f} \\\\ \\hline')
-    return X, X1
+#    # print LaTeX table
+#    for wsp in WSP:
+#        print(f'\multirow{{2}}{{*}} {{{wsp}}} & No IPC &{np.mean(X[wsp]):2.3f} &  {np.std(X[wsp]):2.3f} \\\\')
+#        print(f'& IPC ($C_2$) & {np.mean(X1[wsp]):2.3f} &  {np.std(X1[wsp]):2.3f} \\\\ \\hline')
+    #return X, X1
 
 
 if __name__ is '__main__':
     dlc_noipc = PostProc.DLC('dlc11_0')
     dlc = PostProc.DLC('dlc11_1')
 
-    X, X1 = run(dlc, dlc_noipc, 'ipc04', SAVE=False)
+    run(dlc, dlc_noipc, SAVE=False)
