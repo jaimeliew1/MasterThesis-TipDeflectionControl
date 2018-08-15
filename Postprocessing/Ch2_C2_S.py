3ba1559f96bd9cc8415e8899a5eefd3c168c4298
Jaime Liew <33415790+jaimeliew1@users.noreply.github.com>
2018-08-15 19:26:29 +0200

    sensitivity functions for publication. note: latex is broken

--- Postprocessing/Ch2_C2_S.py
+++ Postprocessing/Ch2_C2_S.py
@@ -15,7 +15,7 @@ import numpy as np
 def run(dlc=None, dlc_noipc=None, SAVE=None):
     f = np.linspace(0, 1.5, 1000)[1:]
     C = []
-    modules = ['IPC_PI', 'IPC04', 'IPC07']
+    modules = ['IPC07']
     for m in modules:
         module = importlib.import_module('Controllers.' + m)
         C.append(module.make)
@@ -26,18 +26,20 @@ def run(dlc=None, dlc_noipc=None, SAVE=None):
         P = BladeModel.Blade(18)
         sys = ControlDesign.Turbine(P, c())
         Mag.append(signal.bode(sys.S, w=f*(2*np.pi))[1])
+        Mag.append(signal.bode(sys.T, w=f*(2*np.pi))[1])
 
     fig, ax = ControllerEvaluation.magplotSetup(F1p=0.16)
+    ax.axhline(0, lw=1, c='0.7')
     ax.set_xlim(0.05, 1.5)
 
-    ax.plot(f, Mag[0], '-.', label='$S(C_{PI})$')
-    ax.plot(f, Mag[1], ':', label='$S(C_{1p})$')
-    ax.plot(f, Mag[2], '-r', label='$S(C_{2})$')
+    ax.plot(f, Mag[0], '-.', label='Sensitivity Function')
+    ax.plot(f, Mag[1], ':', label='Complementary Sensitivity Function')
+    #ax.plot(f, Mag[2], '-r', label='$S(C_{2})$')
 
 
 
 
-    ax.set_ylim(-25, 10)
+    ax.set_ylim(-20, 10)
 
     ax.legend()
 
